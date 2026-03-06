54798336ca7754294f47ecbf106c2ec41e429314
Logan <lbragg98@gmail.com>
2026-03-06 11:47:29 -0500

    lab 2 complete

--- lib/coffee.py
+++ lib/coffee.py
@@ -2,16 +2,20 @@
 
 class Coffee:
     def __init__(self, size, price):
-        self,size = size
+        self.size = size
         self.price = price
 
+    @property
     def size(self):
-        return self.size
+        return self._size
+
+    @size.setter
     def size(self, value):
         if value in ["Small", "Medium", "Large"]:
-            self.size = value
+            self._size = value
         else:
-            print ("size must be in Small, Medium, or Large")
+            print("size must be Small, Medium, or Large")
+
     def tip(self):
-        print("This coffee is great, here's a tip!")
+        print("This coffee is great, here’s a tip!")
         self.price += 1
